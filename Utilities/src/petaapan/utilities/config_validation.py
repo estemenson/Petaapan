b6468c6139980a09e9e8c505b0f2a38d44a7e0e8
Jonathan Gossage <jgossage@gmail.com>
2010-09-18 08:54:19 -0400

    Add module with low-level Argparse support functions

---
+++ Utilities/src/petaapan/utilities/config_validation.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+'''
+Contains assorted general purpose validation classes used when collecting
+arguments from the command line and configuration files.
+
+Created on 2010-09-16
+
+@author: jonathan
+'''
+
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from os.path import isdir, isfile, exists
+from urllib2 import urlopen, URLError
+import logging
+
+import petaapan.utilities.argparse
+
+def ValidateUrl(url):
+    result = logging.NOTSET
+    c = None
+    errmsg = []
+    try:
+        c = urlopen(url)
+        return (result, errmsg)
+    except URLError, ex:
+        result = logging.ERROR
+        errmsg.append(\
+        "%s is an invalid, non-existent or currently inaccessible URL" % (url))
+        errmsg.append(unicode(ex))
+        return (result, errmsg)
+    finally:
+        if c is not None: c.close()
+
+
+def ValidateExistingDirectory(path):
+        result = logging.NOTSET
+        errmsg = []
+        if isdir(path) and exists(path):
+            return (result, errmsg)
+        else:
+            result = logging.ERROR
+            errmsg.append(\
+               '%s must be an existing directory on the current path' % (path))
+            return (result, errmsg)
+
+
+def ValidateExistingFile(path):
+        result = logging.NOTSET
+        errmsg = []
+        if isfile(path) and exists(path):
+            return (result, errmsg)
+        else:
+            result = logging.ERROR
+            errmsg.append(\
+               '%s must be an existing file on the current path' % (path))
+            return (result, errmsg)
