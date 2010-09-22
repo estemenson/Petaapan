c44c8068495c872faac4d724cbea25ca3aa212e9
Jonathan Gossage <jgossage@gmail.com>
2010-09-21 23:42:34 -0400

    Partial editing of notification client

--- PublishSubscribeServer/src/notification.py
+++ PublishSubscribeServer/src/notification.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 '''
 Notification receiver
+Receives notification from github describing the changes that took place in
+the last push to Github. Notification received in the same format as it was
+sent from Github
+Invoked from subscriber application on subscriber's workstation
 
 Created on 2010-09-14
 
@@ -15,3 +19,17 @@ from __future__ import unicode_literals
 
 import json
 import httplib
+import BaseHTTPServer
+
+class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_POST(self):
+        pass
+    
+
+class ServerManager(object):
+    def __init__(self, server_class=BaseHTTPServer.HTTPServer,
+                 handler_class=Handler, host='localhost', port=8080):
+        self._host = host
+        self._port = port
+        self._server = server_class((host, port), handler_class)
+        self._shutdown = False
