62f562424086938106c00466299421601bbbc6a6
Jonathan Gossage <jgossage@gmail.com>
2010-09-26 20:21:14 -0400

    Improve error logging and handling.

--- Utilities/src/petaapan/utilities/sendJsonMsg.py
+++ Utilities/src/petaapan/utilities/sendJsonMsg.py
@@ -16,16 +16,24 @@ from __future__ import unicode_literals
 import json
 import httplib
 import urllib
+import logging
+from petaapan.utilities import reportException
 
-def send(msg, url, dest_host='localhost', dest_port=8080):
+def send(msg, url, dest_host='localhost', dest_port=8080,
+         logger=logging.warning):
+    host = dest_host if dest_host is not None else 'localhost'
+    port = dest_port if dest_port is not None else 8080
     try:
-        conn = httplib.HTTPConnection(dest_host, dest_port)
+        conn = httplib.HTTPConnection(host, port)
         if conn is not None:
             conn.connect()
             conn.request('POST', url, urllib.quote_plus(json.dumps(msg),
                                                         str('/')))
             ret = conn.getresponse()
             return (ret.status, ret.reason, ret.getheaders())
+    except Exception, ex:
+        reportException.report(ex, logger)
+        return (httplib.FAILED_DEPENDENCY, None, None)
     finally:
         if conn is not None: conn.close()
         
