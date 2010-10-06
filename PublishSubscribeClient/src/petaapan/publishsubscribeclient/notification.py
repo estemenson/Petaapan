2d2f839054108b6df9174c3752147056d66bb253
Jonathan Gossage <jgossage@gmail.com>
2010-10-06 18:31:10 -0400

    Restructure PublishSubscribeClient to use the following python packaging structure: petaapan.publishsubscribeclient

--- PublishSubscribeClient/src/petaapan/publishsubscribeclient/notification.py
+++ PublishSubscribeClient/src/petaapan/publishsubscribeclient/notification.py
@@ -21,42 +21,46 @@ import json
 import httplib
 import urllib
 import BaseHTTPServer
-import logging
 import threading
+
+NOTIFICATION = 'Google Notification'
 
 from petaapan.utilities import reportException
 
 class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
+    
     def do_POST(self):
         msg = json.loads(urllib.unquote_plus(self.rfile.read()))
-        try:
+        try: # Send the HTTP response
             self.send_response(httplib.OK)
         except Exception, ex:
-            reportException(ex, logging.error)
-        for o in ServerManager._observers:
-            o(msg)
+            reportException(ex, ServerManager.Log.error
+                                if ServerManager.Log != None else None)
+        # Pass the message up to those who know what to do with it
+        if ServerManager.Response is not None:
+            ServerManager.Response.put((NOTIFICATION, msg), False)
     
 
-class ServerManager(object):
-    _observers = []
+class ServerManager(threading.Thread):
+    Log = None
+    Response = None
     
     def __init__(self, server_class=BaseHTTPServer.HTTPServer,
-                 handler_class=Handler, host='localhost', port=8080):
+                 handler_class=Handler, host='localhost', port=8080,
+                 response_queue=None, log=None):
         self._host = host
         self._port = port
+        ServerManager.Log = log
+        ServerManager.Response = response_queue
         self._server = server_class((host, port), handler_class)
         self._shutdown = False
     
     def run(self): 
         while not self._shutdown:
             self._server.handle_request()       
-        ServerManager._observers = None
         self._server = None
-        
-    def addObserver(self, observer):
-        ServerManager._observers.append(observer)
-    def removeObserver(self, observer):
-        ServerManager._observers.remove(observer)
+        ServerManager.Log = None
+        ServerManager.Response = None
 
     def getShutdown(self):
         return self._shutdown
